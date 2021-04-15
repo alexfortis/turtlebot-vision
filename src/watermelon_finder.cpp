5c9c8ab9956ab78a26b89012456b62d322c74ded
Alex Strong <astrong3@binghamton.edu>
2021-04-15 04:27:05 -0400

    Got the robot to move to all 3 points of interest

--- src/watermelon_finder.cpp
+++ src/watermelon_finder.cpp
@@ -12,64 +12,43 @@ std::ostream &operator<<(std::ostream &os, const Goal &g) {
   return os;
 }
 
-bool moveToGoal(MoveBaseClient &ac, const double &x, const double &y, const double angle=0.0) {
-  move_base_msgs::MoveBaseGoal goal;
-  goal.target_pose.header.frame_id = "map";
-  goal.target_pose.header.stamp = ros::Time::now();
-
-  goal.target_pose.pose.position.x = x;
-  goal.target_pose.pose.position.y = y;
-  goal.target_pose.pose.orientation.w = angle;
-
-#if NAV_DEBUG
-  std::cout << "Sending goal: (" << x << ", " << y << "); " << angle << std::endl;
-#endif
-  ROS_INFO("Sending goal: (%.1f %.1f) at angle %.1f\n", x, y, angle);
-  ac.sendGoal(goal);
-#if NAV_DEBUG
-  std::cout << "Sent goal." << std::endl;
-  bool rslt =
-#endif
-  ac.waitForResult();
-#if NAV_DEBUG
-  std::cout << "Result: " << rslt << std::endl;
-#endif
-  return ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED;
-}
-
-int main(int argc, char **argv) {
-  std::cout << "Watermelon finder not yet implemented" << std::endl;
-  
+int main(int argc, char** argv){
   ros::init(argc, argv, "watermelon_finder");
-  MoveBaseClient action_client("move_base", true);
 
-  double x0 = 5.42, y0 = 2.72,
-    x1 = 4.05, y1 = -4.43,
-    x2 = -6.38, y2 = -1.5;
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  //wait for the action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
   vector<Goal> goals;
-  goals.push_back(make_tuple(5.42, 2.72, 5.0));
+  goals.push_back(make_tuple(5.42, 2.72, 1.0));
   goals.push_back(make_tuple(4.05, -4.43, 1.0));
   goals.push_back(make_tuple(-6.38, -1.5, 1.0));
 
-  bool moved = false;
-  double goal_x, goal_y, goal_angle;
-  
   for(auto it = goals.begin(); it != goals.end(); ++it) {
-    std::tie(goal_x, goal_y, goal_angle) = *it;
-#if NAV_DEBUG
-    std::cout << "Moving to (" << goal_x << ", " << goal_y << ")" << std::endl;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.stamp = ros::Time::now();
     
-#endif
-    moved = moveToGoal(action_client, goal_x, goal_y, goal_angle);
-    if(moved) {
-      ROS_INFO_STREAM("Successfully moved to " << *it);
+    std::tie(goal.target_pose.pose.position.x,
+	     goal.target_pose.pose.position.y,
+	     goal.target_pose.pose.orientation.w) = *it;
+
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
+
+    ac.waitForResult();
+
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("The base successfully moved to (%.2f, %.2f)", std::get<0>(*it), std::get<1>(*it));
       ROS_INFO("%s", found_watermelon()?"True":"False");
     }
-    else {
-#if NAV_DEBUG
-      std::cout << "Failed to move to " << *it << std::endl;
-#endif
-      ROS_INFO_STREAM("Failed to move to " << *it);
-    }
+    else
+      ROS_INFO("The base failed to move to (%.2f, %.2f)", std::get<0>(*it), std::get<1>(*it));
   }
+
+  return 0;
 }
--- src/watermelon_finder.hpp
+++ src/watermelon_finder.hpp
@@ -11,5 +11,3 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 typedef std::tuple<double, double, double> Goal;
 
 bool found_watermelon();
-
-bool moveToGoal(MoveBaseClient &ac, const double &x, const double &y, const double angle);
